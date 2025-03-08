9e9fcee13b56398d7911d996b77d1c7157b30e41
Mariamamr7 <maryam.amr.gamal.helal@gmail.com>
2025-03-08 14:00:11 +0200

    sae chagnge

--- javascript/todolist.js
+++ javascript/todolist.js
@@ -1,30 +1,45 @@
-const inputbox=document.getElementById('input_box');
-const list_conainer=document.getElementById('list_conainer');
+const input_box=document.getElementById('input_box');
+const  listcotainer=document.getElementById('listcotainer');
 
 
-function  addtask()
+function add_task()
 {
-    if(inputbox.value==='')
-    {
-       alert('you should enter task');
-    }else{
-        let li=document.createElement('li');
-        li.innerHTML=inputbox.value;
-        list_conainer.appendChild(li);
-        let span=document.createElement('span');
-        //Multiplication Sign  Unicode Character
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
-    }
-    inputbox.value="";
+  task=document.getElementById('input_box').value;
+
+  if(task=="")
+  {
+    alert('please enter the task');
+  }
+  else{
+     var li=document.createElement('li');
+  
+     li.innerHTML=input_box.value;
+    // <li>task4</li>
+    listcotainer.appendChild(li);
+    //<span> X</span>
+
+    var span=document.createElement('span');
+      span.innerHTML="\u00d7";
+      li.appendChild(span);
+  }
+ 
+  document.getElementById('input_box').value="";
 }
 
-list_conainer.addEventListener('click',function(e){
-    if(e.target.tagName==='LI')
-    {
-        e.target.classList.toggle('checked');
-    }else if(e.target.tagName==='SPAN')
-    {
-        e.target.parentElement.remove();
-    }
-})
+listcotainer.addEventListener('click',function(e){
+
+
+  if(e.target.tagName==="LI")
+  {
+
+    e.target.classList.toggle('checked');
+  }
+   else if(e.target.tagName==="SPAN")
+   {
+
+    e.target.parentElement.remove();
+   }
+  
+});
+ 
+
